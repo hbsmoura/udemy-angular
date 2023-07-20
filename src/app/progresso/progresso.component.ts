481075989c6a3ea38ea06b15a2c0f2864a7225ab
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-07-20 17:05:03 -0300

    barra de progresso

---
+++ src/app/progresso/progresso.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progresso',
+  templateUrl: './progresso.component.html',
+  styleUrls: ['./progresso.component.css']
+})
+export class ProgressoComponent {
+
+    @Input() progresso = 0
+
+}
